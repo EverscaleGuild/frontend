88b9c8524e8ecf5fd2412b59b7d3d517a87f47bb
yura <yurchik.boyko@gmail.com>
2022-07-16 23:17:23 +0200

    logout button working, new progile pic

--- src/pages/bets.tsx
+++ src/pages/bets.tsx
@@ -37,7 +37,7 @@ const ever = new ProviderRpcClient();
 
 export default function ActionAreaCard() {
     const [bets, setBets] = React.useState<any[]>([]);
-    
+
     const [game, setGame] = React.useState<string | null>(null);
     const loadBets = async () => {
       await ever.ensureInitialized();
@@ -66,8 +66,9 @@ export default function ActionAreaCard() {
                     <CardMedia
                         component="img"
                         height="160"
-                        image={`https://avatars.dicebear.com/api/identicon/${item[1].hash}.svg?background=%23${item[1].beat ? "39f491" : "FF6922"}`}
-                        alt="green iguan"
+                        //`https://avatars.dicebear.com/api/identicon/${item[1].hash}.svg?background=%23
+                         image={item[1].beat ? "https://avatars.dicebear.com/api/pixel-art/d.svg?background=%2339f491" : `https://avatars.dicebear.com/api/identicon/${item[1].hash}.svg?background=%23FF6922`}
+                         alt="green iguan"
                     />
                     <CardContent>
                         <Typography gutterBottom variant="h5" component="div">
