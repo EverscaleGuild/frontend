50a380c876119ec9ec611fbc16c8e2a4fca3d736
yura <yurchik.boyko@gmail.com>
2022-07-16 14:30:22 +0200

    new ui changes

--- src/pages/bets.tsx
+++ src/pages/bets.tsx
@@ -5,6 +5,12 @@ import CardMedia from '@mui/material/CardMedia';
 import Typography from '@mui/material/Typography';
 import { CardActionArea, Box } from '@mui/material';
 import { Game } from '../components/Game';
+import AddIcon from '@mui/icons-material/Add';
+import Button from '@mui/material/Button';
+import Modal from '@mui/material/Modal';
+import TextField from '@mui/material/TextField';
+import Autocomplete from '@mui/material/Autocomplete';
+import Paper from '@mui/material/Paper';
 import { CreateBet } from '../components/CreateBet';
 import './bets.css';
 
